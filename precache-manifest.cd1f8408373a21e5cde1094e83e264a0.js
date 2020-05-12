10e03e87ede222e9d7bd871de50f664ad13876d9
Adam Sheaffer <adam.e.sheaffer@gmail.com>
2020-05-12 17:57:14 -0500

    Updates

---
+++ precache-manifest.cd1f8408373a21e5cde1094e83e264a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20a4d0eca74c2d2ae8bfa0bece13a7d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3bc84f186f36216a2fdb",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "679d8602b445f0235663",
+    "url": "/static/css/main.bedc073b.chunk.css"
+  },
+  {
+    "revision": "3bc84f186f36216a2fdb",
+    "url": "/static/js/2.df878971.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.df878971.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "679d8602b445f0235663",
+    "url": "/static/js/main.a9a20565.chunk.js"
+  },
+  {
+    "revision": "38f05ac45fc090c9f4ce",
+    "url": "/static/js/runtime-main.706947a8.js"
+  }
+]);
